9a811384b84998b9d2ff78192183e4e38a818584
digital-pers0n <aquaphonic@gmail.com>
2018-11-02 22:34:09 +0300

    Remove @autoreleasepool

--- aerodrome-cli/main.m
+++ aerodrome-cli/main.m
@@ -41,212 +41,196 @@ usage()
 }
 
 int main(int argc, char * argv[]) {
-    @autoreleasepool {
+    
+    int             ch;
+    int             channel = 0;
+    boolean_t		disassociate = FALSE;
+    boolean_t       info = FALSE;
+    const char *	if_name = NULL;
+    const char *	pass = NULL;
+    const char *	network = NULL;
+    const char *    ibss = NULL;
+    boolean_t       power = FALSE;
+    boolean_t       scan = FALSE;
+    Apple80211Ref   ref = NULL;
+    boolean_t       test = FALSE;
+    uint32_t        locale = 0;
+    boolean_t       dump_rom = FALSE;
+    boolean_t       hw_info = FALSE;
+    boolean_t       rate = FALSE;
+    uint32_t        setrate = 0;
+    uint32_t        set_powersave = 0;
+    uint32_t        set_tx = 0;
+    
+    if (argc < 2) {
+        usage();
         
-        int             ch;
-        int             channel = 0;
-        boolean_t		disassociate = FALSE;
-        boolean_t       info = FALSE;
-        const char *	if_name = NULL;
-        const char *	pass = NULL;
-        const char *	network = NULL;
-        const char *    ibss = NULL;
-        boolean_t       power = FALSE;
-        boolean_t       scan = FALSE;
-        Apple80211Ref   ref = NULL;
-        boolean_t       test = FALSE;
-        uint32_t        locale = 0;
-        boolean_t       dump_rom = FALSE;
-        boolean_t       hw_info = FALSE;
-        boolean_t       rate = FALSE;
-        uint32_t        setrate = 0;
-        uint32_t        set_powersave = 0;
-        uint32_t        set_tx = 0;
-        
-        if (argc < 2) {
-            usage();
-            
+    }
+    while ((ch =  getopt(argc, argv, "DdpwshHITi:a:n:k:c:l:t:P:X:")) != EOF) {
+        switch ((char)ch) {
+            case 'h':
+            case 'H':
+                usage();
+                exit(0);
+                break;
+            case 'n':		/* join network */
+                network = optarg;
+                break;
+            case 's':		/* scan for wireless netork */
+                scan = TRUE;
+                break;
+            case 'd':
+                disassociate = TRUE;
+                break;
+            case 'i':		/* specify the interface */
+                if_name = optarg;
+                break;
+            case 'p':		/* switch power */
+                power = TRUE;
+                break;
+            case 'k':		/* specify the password for associate */
+                pass = optarg;
+                break;
+            case 'a':		/* specify the IBSS name */
+                ibss = optarg;
+                break;
+            case 'c':		/* set channel */
+                channel = atoi(optarg);
+                break;
+            case 'I':
+                info = TRUE;
+                break;
+            case 'l':
+                locale = atoi(optarg);
+                break;
+            case 'D':
+                dump_rom = TRUE;
+                break;
+            case 'w':
+                hw_info = TRUE;
+                break;
+            case 'T':
+                rate = TRUE;
+                break;
+            case 't':
+                setrate = atoi(optarg);
+                break;
+            case 'P':
+                set_powersave = atoi(optarg);
+                break;
+            case 'X':
+                set_tx = atoi(optarg);
+                break;
+            default:
+                break;
         }
-        while ((ch =  getopt(argc, argv, "DdpwshHITi:a:n:k:c:l:t:P:X:")) != EOF) {
-            switch ((char)ch) {
-                case 'h':
-                case 'H':
-                    usage();
-                    exit(0);
-                    break;
-                case 'n':		/* join network */
-                    network = optarg;
-                    break;
-                case 's':		/* scan for wireless netork */
-                    scan = TRUE;
-                    break;
-                case 'd':
-                    disassociate = TRUE;
-                    break;
-                case 'i':		/* specify the interface */
-                    if_name = optarg;
-                    break;
-                case 'p':		/* switch power */
-                    power = TRUE;
-                    break;
-                case 'k':		/* specify the password for associate */
-                    pass = optarg;
-                    break;
-                case 'a':		/* specify the IBSS name */
-                    ibss = optarg;
-                    break;
-                case 'c':		/* set channel */
-                    channel = atoi(optarg);
-                    break;
-                case 'I':
-                    info = TRUE;
-                    break;
-                case 'l':
-                    locale = atoi(optarg);
-                    break;
-                case 'D':
-                    dump_rom = TRUE;
-                    break;
-                case 'w':
-                    hw_info = TRUE;
-                    break;
-                case 'T':
-                    rate = TRUE;
-                    break;
-                case 't':
-                    setrate = atoi(optarg);
-                    break;
-                case 'P':
-                    set_powersave = atoi(optarg);
-                    break;
-                case 'X':
-                    set_tx = atoi(optarg);
-                    break;
-                default:
-                    break;
-                    
-                    
-            }
-        }
-        
-
-
-
-        // Bind to interface
-        if (if_name != NULL) {
-            g_if_name = (char *)if_name;
-            if (aerodrome_bind(if_name, &ref) == FALSE)
-            {
-                printf("interface '%s' is not present or not AirPort\n",
-                       if_name);
-                exit(1);
-            }
-        } else if (if_name == NULL)
+    }
+    
+    // Bind to interface
+    if (if_name != NULL)
+    {
+        strncpy(g_if_name, if_name, IF_NAMESIZE);
+        if (aerodrome_bind(if_name, &ref) == FALSE)
         {
-            g_if_name = (char *)aerodrome_auto_bind(&ref);
+            fprintf(stderr, "Interface '%s' is not present or not AirPort\n",
+                    if_name);
+            exit(EXIT_FAILURE);
         }
-        
-        
-        // Associate with network
-        
-        if (network != NULL) {
-
-            fprintf(stderr, "attempting to join network '%s'...\n", network);
-            
-            if (aerodrome_join_network(ref, network, pass) == FALSE) {
-                //fprintf(stderr, "aerodrome_join_network failed\n");
-            }
+    } else {
+        if (!aerodrome_auto_bind(&ref)) {
+            fputs("Invalid interface\n", stderr);
+            exit(EXIT_FAILURE);
         }
+    }
+    
+    
+    // Associate with network
+    
+    if (network != NULL) {
         
-
-        // Create computer-to-computer network
+        fprintf(stderr, "attempting to join network '%s'... ", network);
         
-        if (ibss != NULL) {
-            
-            fprintf(stderr, "attempting to create network '%s'...\n", ibss);
-            
-            if(aerodrome_ibss_create(ref, ibss, channel) == FALSE){
-                
-                //fprintf(stderr, "aerodrome_ibss_create failed\n");
-            }
-            
-            
-            
+        if (aerodrome_join_network(ref, network, pass)) {
+            puts("Done.");
         }
+    }
+    
+    
+    // Create computer-to-computer network
+    
+    if (ibss != NULL) {
         
-        // Switch power on/off
+        fprintf(stderr, "attempting to create network '%s'... ", ibss);
         
-        if (power) {
-            aerodrome_power_cycle(ref);
+        if(aerodrome_ibss_create(ref, ibss, channel)){
+            puts("Done.");
         }
+    }
+    
+    // Switch power on/off
+    
+    if (power) {
+        power_cycle();
+    }
+    
+    
+    // Disassociate from current network
+    if (disassociate) {
+        aerodrome_disassociate(ref);
+    }
+    
+    if (info) {
+        get_info();
+    }
+    
+    if (scan) {
+        aerodrome_scan(ref);
+    }
+    
+    if (test) {
+        // aerodrome_test(ref);
+    }
+    
+    if (locale > 0) {
+        set_locale(locale);
+        printf("Locale: %s \n", get_locale());
+    }
+    
+    if (dump_rom) {
+        aerodrome_dump_rom(ref);
+    }
+    
+    if (hw_info) {
+        get_hardware_info();
+    }
+    
+    if (rate) {
+        get_supported_rate();
+    }
+    
+    if (setrate > 0) {
         
+        set_rate(setrate);
+        printf("txRate: %i Mbps\n", get_rate());
         
-        // Disassociate from current network
-        if (disassociate) {
-            aerodrome_disassociate(ref);
-        }
+    }
+    
+    if (set_powersave > 0) {
         
-        if (info) {
-            get_info();
-        }
+        char * mode;
         
-        if (scan) {
-            aerodrome_scan(ref);
-        }
-        
-        if (test) {
-           // aerodrome_test(ref);
-        }
-        
-        if (locale > 0) {
-            set_locale(locale);
-            printf("Locale: %s \n", get_locale());
-        }
-        
-        if (dump_rom) {
-            aerodrome_dump_rom(ref);
-        }
-        
-        if (hw_info) {
-            get_hardware_info();
-        }
-        
-        if (rate) {
-            get_supported_rate();
-        }
-        
-        if (setrate > 0) {
-            
-            set_rate(setrate);
-            printf("txRate: %i Mbps\n", get_rate());
-            
-        }
-        
-        if (set_powersave > 0) {
-            
-            char * mode;
-            
-            set_powersave_mode(set_powersave);
-            get_powersave_mode(&mode);
-            printf("Powersave Mode: %s\n", mode);
-        }
-        
-        if (set_tx > 0) {
-            set_txpower(set_tx);
-            printf("txPower: %i mW\n", get_txpower());
-        }
-        
-        //        if (channel > 0) {
-        //
-        //            wireless_set_channel(ref, channel);
-        //
-        //        }
-        
-        if (if_name !=  NULL) {
-            aerodrome_close(ref);
-        }
-        
-        
+        set_powersave_mode(set_powersave);
+        get_powersave_mode(&mode);
+        printf("Powersave Mode: %s\n", mode);
+    }
+    
+    if (set_tx > 0) {
+        set_txpower(set_tx);
+        printf("txPower: %i mW\n", get_txpower());
+    }
+    
+    if (ref !=  NULL) {
+        aerodrome_close(ref);
     }
     return 0;
 }
